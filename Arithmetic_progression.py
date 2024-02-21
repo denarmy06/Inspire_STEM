50dee7aa314911667eb54277850274bc69c3a6d3
denarmy06 <159755125+denarmy06@users.noreply.github.com>
2024-02-21 09:40:29 +0300

    Add files via upload

--- Arithmetic_progression.py
+++ Arithmetic_progression.py
@@ -10,7 +10,7 @@ n = float(input("Enter the position of the term :"))
 sol = (a + (n - 1)*d) 
 sum = ((n/2)*((2*a) + ((n - 1) * d)))
 
-print("The value of the term is :",sol)
-print("The sum of the terms are :",sum)
+print("The value of the nth term is :",sol)
+print("The sum up to the nth terms is :",sum)
 
 
