5e8b36ff85d22da40c9172529b7e70c7e59ec45f
denarmy06 <159755125+denarmy06@users.noreply.github.com>
2024-02-26 10:04:31 +0300

    Add files via upload

---
+++ Project_3/reversing_string.py
@@ -0,0 +1,14 @@
+# Program to reverce string
+# Date: 22/02/2024
+# Name: Dennis Muhami
+
+string = input("Enter string :")
+print(string)
+
+print("\t")
+
+new_string = (string[::-1])
+print(new_string)
+
+
+
