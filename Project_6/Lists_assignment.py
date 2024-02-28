a3fe52f2a881416d5d8dbb18b711acf3f198375d
denarmy06 <ndamburidennis2006@gmail.com>
2024-02-28 20:28:07 +0300

    upto Lists_assignments.py

---
+++ Project_6/Lists_assignment.py
@@ -0,0 +1,23 @@
+# Date: 27/02/2024
+# Name: Dennis Muhami
+
+# Tuples
+hobbies = ("Gaming","Surfing the internet","Litsening to music","Swimming")
+print("My favourite hobbies are :",hobbies)
+print("\n")
+
+# Dictionaries
+car = {"Type":"Ford Mustang","Color":"Matte black","D.O.M":"2021"}
+print("Here's info about my dream car :",car)
+print("\t")
+
+print(car["Type"])
+print(car["Color"])
+print(car["D.O.M"])
+print("\t")
+
+# Copy
+vehicle = car
+print(vehicle)
+
+
