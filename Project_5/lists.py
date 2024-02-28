150d432bfbde9a34a16c7cdbe471ef60cb535c8f
denarmy06 <ndamburidennis2006@gmail.com>
2024-02-28 08:30:15 +0300

    lists assignment

---
+++ Project_5/lists.py
@@ -0,0 +1,26 @@
+# project for lists
+# Date: 27/02/2024
+# Name: Dennis Muhami
+
+items = ["Demon slayer","Fate","Attack on titan","Solo leveling","jobless reincarnation"]
+print("My top 5 favourite animes are :",items)
+print("\n")
+
+items.pop(0)
+items.pop(3)
+print("My top 3 favourites are :",items)
+print("\n")
+
+items.sort()
+print("In order, they are :",items)
+print("\n")
+
+items.reverse()
+print("from the latest made :",items)
+print("\n")
+
+items.count(0)
+print(len(items))
+
+
+
